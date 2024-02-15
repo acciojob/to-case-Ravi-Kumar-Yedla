5b9e705f56ea10ea74fafb9cfbae247228523ad3
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-15 09:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toCase(text) {
   // write your code here
+	  const lower =  text.tolowerCase();
+     const upper = text.toupperCase();
+	return lower +"-"+ upper;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
